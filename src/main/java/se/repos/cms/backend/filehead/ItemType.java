6996ca08af0a6ee06f47ee416c16ebf2d231e913
hsvallfors <hsvallfors>
2013-12-20 10:22:39 +0000

    Fixed trunk fix.

---
+++ src/main/java/se/repos/cms/backend/filehead/ItemType.java
@@ -0,0 +1,8 @@
+/**
+ * Copyright (C) Repos Mjukvara AB
+ */
+package se.repos.cms.backend.filehead;
+
+public enum ItemType {
+    FILE, FOLDER, BOTH
+}
---
+++ src/main/java/se/repos/cms/backend/filehead/LocalRepoRevision.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) Repos Mjukvara AB
+ */
+package se.repos.cms.backend.filehead;
+
+import java.util.Date;
+
+import se.simonsoft.cms.item.RepoRevision;
+
+public class LocalRepoRevision extends RepoRevision {
+
+    protected LocalRepoRevision(Date revisionTimestamp) {
+        super(revisionTimestamp);
+    }
+}
